c1129836353e85c21b16de81f9bffdd9b78e515f
neutmute <neutmute@neutmute.nm>
2015-07-14 22:35:47 +1000

    prepare docs

--- Neo4jClient.Extension.Test/TestData/Entities/Weapon.cs
+++ Neo4jClient.Extension.Test/TestData/Entities/Weapon.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Neo4jClient.Extension.Test.TestData.Entities
-{
-    public class Weapon
-    {
-        public int Id { get; set; }
+//namespace Neo4jClient.Extension.Test.TestData.Entities
+//{
+//    public class Weapon
+//    {
+//        public int Id { get; set; }
 
-        public string Name { get; set; }
-    }
-}
+//        public string Name { get; set; }
+//    }
+//}
