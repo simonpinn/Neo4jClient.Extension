c33b9f5dbfb71d4f2603f38f7f18c9057a3389e1
neutmute <neutmute.email>
2015-07-15 21:45:46 +1000

    commands to manually package

---
+++ _package/_package.cmd
@@ -0,0 +1,3 @@
+del /q *.nupkg
+..\.nuget\nuget.exe pack ..\Neo4jClient.Extension\Neo4jClient.Extension.csproj -IncludeReferencedProjects -Prop Configuration=%1
+..\.nuget\nuget.exe pack ..\Neo4jClient.Extension.Attributes\Neo4jClient.Extension.Attributes.csproj -IncludeReferencedProjects -Prop Configuration=%1
