1a07c44bef96eb0faf1d6c130fff21c6e70c5daa
neutmute <neutmute@neutmute.nm>
2015-07-21 15:34:50 +1000

    nuget publish create

--- SolutionItems/Properties/AssemblyInfoGlobal.cs
+++ SolutionItems/Properties/AssemblyInfoGlobal.cs
@@ -4,10 +4,10 @@ using System.Runtime.InteropServices;
 
 
 [assembly: AssemblyCopyright("Copyright © 2014")]
-[assembly: AssemblyVersion("0.1.2.0")]
+[assembly: AssemblyVersion("0.1.2.1")]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
-[assembly: AssemblyInformationalVersion("0.1.2.0-beta-tx")]    // trigger pre release package
+[assembly: AssemblyInformationalVersion("0.1.2.1-beta-tx")]    // trigger pre release package
 #else
     [assembly: AssemblyConfiguration("Release")]
 #endif
---
+++ _package/setapikey.cmd
@@ -0,0 +1,2 @@
+REM Pass api key as arg1
+..\.nuget\nuget.exe setapikey %1 -Source https://www.myget.org/F/neo4jclient-extension-tx/api/v2/package
