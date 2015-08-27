0d63221be0a8d6fcc7587b1e1ba5af0321e0e163
cormacdebarra <cdebarra@bauer-media.com.au>
2015-08-27 14:22:28 +1000

    updated assembly version

--- SolutionItems/Properties/AssemblyInfoGlobal.cs
+++ SolutionItems/Properties/AssemblyInfoGlobal.cs
@@ -4,10 +4,10 @@ using System.Runtime.InteropServices;
 
 
 [assembly: AssemblyCopyright("Copyright © 2014")]
-[assembly: AssemblyVersion("0.1.3.1")]
+[assembly: AssemblyVersion("0.1.3.3")]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
-[assembly: AssemblyInformationalVersion("0.1.3.2-beta")]    // trigger pre release package
+[assembly: AssemblyInformationalVersion("0.1.3.3-beta")]    // trigger pre release package
 #else
     [assembly: AssemblyConfiguration("Release")]
 #endif
